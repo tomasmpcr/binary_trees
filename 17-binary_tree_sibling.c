73e76875ef11f82db97bc147b147c0e66a1cbd25
mstom332 <tomas@332.com.uy>
2021-07-19 16:45:37 +0000

    Adding 16, 17, 18 Co-authored-by: Ksualboy <Ksualboy@users.noreply.github.com>

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: node to evaluate
+ * ------------------------------------
+ * Return: node bro
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (NULL);
+
+	if (node == node->parent->left)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
