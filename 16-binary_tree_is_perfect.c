985ddb3fd32de6ba1811db9b276dd14646077578
mstom332 <tomas@332.com.uy>
2021-07-22 13:27:50 +0000

    Fixing 16
    Co-authored-by: Ksualboy <Ksualboy@users.noreply.github.com>

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,7 @@
 #include "binary_trees.h"
 
 void loop_trough_height(const binary_tree_t *node, int *num, int level);
+void loop_trough_size(const binary_tree_t *node, int *num);
 
 /**
  * binary_tree_is_perfect - Checks if a tree is perfect
@@ -13,14 +14,23 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	int height_left = 0;
 	int height_right = 0;
 
+	int size_left = 0;
+	int size_right = 0;
+
 	if (!tree)
 		return (0);
 
 	loop_trough_height(tree->left, &height_left, 1);
 	loop_trough_height(tree->right, &height_right, 1);
 
+	loop_trough_size(tree->left, &size_left);
+	loop_trough_size(tree->right, &size_right);
+
 	if (height_left == height_right)
-		return (1);
+		if (size_left == size_right)
+			return (1);
+		else
+			return (0);
 	else
 		return (0);
 }
@@ -45,4 +55,24 @@ void loop_trough_height(const binary_tree_t *node, int *num, int level)
 
 	if ((*num) < level)
 		*num = level;
+}
+
+/**
+ * loop_trough_size - Calculates the level of the tree
+ * @node: Tree to calculate
+ * @num: pointer for recursive
+ * ------------------------------------
+ */
+void loop_trough_size(const binary_tree_t *node, int *num)
+{
+	if (!node)
+		return;
+
+	if (node->left)
+		loop_trough_size(node->left, num);
+
+	if (node->right)
+		loop_trough_size(node->right, num);
+
+	*num = *num + 1;
 }
