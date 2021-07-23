b4c9a999ed86fa2a8372406793095a974e014e25
Daniel Millan <77964799+Ksualboy@users.noreply.github.com>
2021-07-23 10:18:00 -0300

    fixing prototype

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,7 +1,5 @@
 #include "binary_trees.h"
 
-size_t binary_tree_depth(const binary_tree_t *tree);
-
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
 
